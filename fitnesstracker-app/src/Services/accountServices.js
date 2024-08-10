ae441d46294426e2ce6b5058bbcb55cc980abf16
DimitarAtanassov <dimitar789558@gmail.com>
2024-08-09 20:29:40 -0700

    Code Refactor: Account Service update

--- fitnesstracker-app/src/Services/accountServices.js
+++ fitnesstracker-app/src/Services/accountServices.js
@@ -1,42 +1,41 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5162/api/accout/";
+const API_BASE_URL = "http://localhost:5162/api/account/";
 
 const apiService = axios.create({
   baseURL: API_BASE_URL,
 });
 
-const AccountService = () => {
-
-  const login = async (username, password) => {
-    const response = await apiService
-      .post("login", {
-        "username": username,
-        "password": password,
-      })
-      .then((res) => {
-        localStorage.setItem("token", response.data.token);
-      })
-      .catch((err) => {
-        console.log(err);
+const AccountService = {
+  login: async (username, password) => {
+    try {
+      const response = await apiService.post("login", {
+        username: username,
+        password: password,
       });
-  };
+      localStorage.setItem("token", response.data.token);
+      return response; // Return the response for further processing if needed
+    } catch (err) {
+      console.error("Error during login:", err);
+      throw err; // Re-throw the error to handle it in the calling function
+    }
+  },
 
-  const register = async (username, password,email) => {
-    const response = await apiService
-      .post("register", {
-        "username": username,
-        "password": password,
-        "email": email
-      })
-      .then((res) => {
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem('username', response.data.username);
-    })
-      .catch((err) => {
-        console.log(err);
+  register: async (username, password, email) => {
+    try {
+      const response = await apiService.post("register", {
+        username: username,
+        password: password,
+        email: email,
       });
-  };
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("username", response.data.username);
+      return response; // Return the response for further processing if needed
+    } catch (err) {
+      console.error("Error during registration:", err);
+      throw err; // Re-throw the error to handle it in the calling function
+    }
+  },
 };
 
 export default AccountService;
